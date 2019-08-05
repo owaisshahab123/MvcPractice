f369052c5ce4e4fe29b9ece1b8a9bb97cfc5aed6
OWAIS_VIFTECH <owais.viftech@gmail.com>
2019-08-06 03:51:29 +0500

    First Crud Perform Using ADO.Net

---
+++ MvcPracticeApplication/Controllers/StudentController.cs
@@ -0,0 +1,96 @@
+﻿using MvcPracticeApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcPracticeApplication.Controllers
+{
+
+    public class StudentController : Controller
+    {
+
+        // 1. *************RETRIEVE ALL STUDENT DETAILS ******************
+        // GET: Student
+        public ActionResult Index()
+        {
+            StudentDBHandle dbhandle = new StudentDBHandle();
+            ModelState.Clear();
+            return View(dbhandle.GetStudent());
+        }
+
+        // 2. *************ADD NEW STUDENT ******************
+        // GET: Student/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        [HttpPost]
+        public ActionResult Create(StudentModel smodel)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    StudentDBHandle sdb = new StudentDBHandle();
+                    if (sdb.AddStudent(smodel))
+                    {
+                        ViewBag.Message = "Student Details Added Successfully";
+                        ModelState.Clear();
+                    }
+                }
+                return View();
+            }
+            catch(Exception ex)
+            {
+                return View();
+            }
+        }
+
+        // 3. ************* EDIT STUDENT DETAILS ******************
+        // GET: Student/Edit/5
+        public ActionResult Edit(int id)
+        {
+            StudentDBHandle sdb = new StudentDBHandle();
+            return View(sdb.GetStudent().Find(smodel => smodel.Id == id));
+        }
+
+        // POST: Student/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, StudentModel smodel)
+        {
+            try
+            {
+                StudentDBHandle sdb = new StudentDBHandle();
+                sdb.UpdateDetails(smodel);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // 4. ************* DELETE STUDENT DETAILS ******************
+        // GET: Student/Delete/5
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                StudentDBHandle sdb = new StudentDBHandle();
+                if (sdb.DeleteStudent(id))
+                {
+                    ViewBag.AlertMsg = "Student Deleted Successfully";
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
