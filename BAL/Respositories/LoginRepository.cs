78472b474fe6dc556e5927e5c9bbf4e0c5fbacce
owaisshahab123 <owaisshahab@hotmail.com>
2019-08-26 02:29:24 +0500

    some changes 2

---
+++ BAL/Respositories/LoginRepository.cs
@@ -0,0 +1,65 @@
+﻿using DAL.DBEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViewModel.Model;
+
+namespace BAL.Respositories
+{
+    public class LoginRepository : BaseRepository
+    {
+        public LoginRepository()
+            : base()
+        {
+
+        }
+
+        public LoginRepository(MvcPracticeEntities ContextDB)
+            : base(ContextDB)
+        {
+
+            DBContext = ContextDB;
+            //DBContext.Database.Connection.Open();
+        }
+
+        public UserProfile GetUserProfile(string uid, string pass)
+        {
+            var password = Common.Encrypt(pass);
+            return DBContext.UserProfiles.Where(x => x.IsActive == true && x.Deleted_At == null && x.Email.Trim().ToLower().Replace(" ", "") == uid.Trim().ToLower().Replace(" ", "") && x.Password == password).FirstOrDefault();
+        }
+        public UserProfile UserByID(int uid)
+        {
+            return DBContext.UserProfiles.Where(x => x.ID == uid).FirstOrDefault();
+        }
+        public UserProfile GetUserContractorProfileByRoleID(string UName, string pass, int roleid)
+        {
+            //ErrorHandling.WriteError("DBContext:"+ DBContext.Database.Connection.ConnectionString);
+            // int contrID = vt_Common.Contractor_ID();
+            var password = Common.Encrypt(pass);
+            return DBContext.UserProfiles.Where(x => x.IsActive == true && x.Deleted_At == null && x.Fisrt_Name.Trim().ToLower().Replace(" ", "") == UName.Trim().ToLower().Replace(" ", "") && x.Password == password && x.Role_ID == roleid).FirstOrDefault();
+        }
+        public UserProfile GetUserEngProfileByRoleID(string Email, string pass, int roleid)
+        {
+            var password = Common.Encrypt(pass);
+            return DBContext.UserProfiles.Where(x => x.IsActive == true && x.Deleted_At == null && x.Email.Trim().ToLower().Replace(" ", "") == Email.Trim().ToLower().Replace(" ", "") && x.Password == password && x.Role_ID == roleid).FirstOrDefault();
+        }
+
+
+        public List<UserPermission> GetUserPermission(int UserID, int RoleID)
+        {
+            //var abc = DBContext.vt_UserPermissions.Where(x => x.UserID == UserID).ToList();
+
+            if (RoleID == 1)
+            {
+                return DBContext.UserPermissions.Where(x => x.ID == UserID).ToList();
+            }
+            else
+            {
+                return DBContext.UserPermissions.Where(x => x.Role == RoleID).ToList();
+            }
+
+        }
+    }
+}
---
+++ MvcApiApplication/Controllers/LoginController.cs
@@ -0,0 +1,133 @@
+﻿using BAL.Respositories;
+using DAL.DBEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using ViewModel.Model;
+using static ViewModel.ViewModel;
+using static ViewModel.VM;
+
+namespace MvcApiApplication.Controllers
+{
+    public class LoginController : ApiController
+    {
+        Session Cpmmonsession = new Session();
+
+        LoginRepository loginrepo;
+        UserRepository userrepo;
+        public LoginController()
+        {
+            loginrepo = new LoginRepository(new MvcPracticeEntities());
+            userrepo = new UserRepository(new MvcPracticeEntities());
+        }
+
+        // GET: Login
+        [Route("api/Login/ValidateLogin")]
+        [HttpPost]
+        public GetLoginResponse ValidateLogin(UserProfileCustom User)
+        {
+            var data = Common.Encrypt(User.User_ID);
+
+
+            GetLoginResponse Response = new GetLoginResponse();
+
+            APIResponseHeader Header = new APIResponseHeader();
+            try
+            {
+                var user = loginrepo.GetUserProfile(User.User_ID, User.Password);
+
+                if (user != null)
+                {
+                    if (User.DeviceToken != null && User.DeviceToken != "" && User.DeviceType != null && User.DeviceType != "")
+                    {
+
+                    }
+                }
+
+                if (User.isWeb == true)
+                {
+
+                    if (user != null)
+                    {
+
+                        Cpmmonsession.AccessToken = "abc";
+                        Header.AccessToken = "abc";
+                        Header.IsSuccess = true;
+                        Header.Message = "Record Found";
+                        Response.User = user;
+                        Response.Permissions = loginrepo.GetUserPermission(Convert.ToInt32(user.ID), Convert.ToInt32(user.Role_ID));
+                    }
+                    else
+                    {
+                        Header.IsSuccess = false;
+                        Header.Message = "Record Not Found";
+
+                    }
+
+
+                }
+                else
+                {
+                    if (user != null)
+                    {
+
+                        Cpmmonsession.AccessToken = "abc";
+                        Header.AccessToken = "abc";
+                        Header.IsSuccess = true;
+                        Header.Message = "Record Found";
+                        Response.User = user;
+
+                    }
+                    else
+                    {
+                        Header.IsSuccess = true;
+                        Header.Message = "Record Not Found";
+
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Header.IsSuccess = false;
+                Header.Message = ex.Message;
+            }
+            Response.Header = Header;
+            return Response;
+        }
+
+        [Route("api/Login/MobUserLogout")]
+        [HttpPost]
+        public GetLoginResponse MobUserLogout(UserProfileCustom User)
+        {
+
+            GetLoginResponse Response = new GetLoginResponse();
+
+            APIResponseHeader Header = new APIResponseHeader();
+            try
+            {
+                var user = loginrepo.UserByID(Convert.ToInt32(User.User_ID));
+                if (user != null)
+                {
+                    Header.IsSuccess = true;
+                    Header.Message = "Record Updated successfully.";
+                }
+                else
+                    throw new Exception("");
+
+
+            }
+            catch (Exception)
+            {
+                Header.IsSuccess = false;
+                Header.Message = "Exception occured when logout.";
+            }
+            Response.Header = Header;
+            return Response;
+        }
+
+    }
+}
