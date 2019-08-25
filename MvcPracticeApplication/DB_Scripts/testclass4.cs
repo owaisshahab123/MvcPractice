25626ee1f44c20acfcfa02cf25396faef11c9d9c
owaisshahab123 <owaisshahab@hotmail.com>
2019-08-26 03:21:42 +0500

    testclss4

---
+++ MvcPracticeApplication/DB_Scripts/testclass4.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcPracticeApplication.DB_Scripts
+{
+    public class testclass4
+    {
+    }
+}
