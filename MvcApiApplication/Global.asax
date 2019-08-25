0e9dc22a392753360e66a4169ce0872342422478
owaisshahab123 <owaisshahab@hotmail.com>
2019-08-26 02:39:29 +0500

    some more changes 3

---
+++ MvcApiApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcApiApplication.WebApiApplication" Language="C#" %>
---
+++ MvcApiApplication/Global.asax.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace MvcApiApplication
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        public void ClearApplicationCache()
+        {
+            List<string> keys = new List<string>();
+
+            // retrieve application Cache enumerator
+            IDictionaryEnumerator enumerator = HttpContext.Current.Cache.GetEnumerator();
+
+            // copy all keys that currently exist in Cache
+            while (enumerator.MoveNext())
+            {
+                keys.Add(enumerator.Key.ToString());
+            }
+
+            // delete every key from cache
+            for (int i = 0; i < keys.Count; i++)
+            {
+                HttpContext.Current.Cache.Remove(keys[i]);
+            }
+        }
+    }
+}
